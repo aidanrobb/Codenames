4d36b77e3759d9a490467944e88473cb0fb25096
judymei <judymei@10.84.141.19>
2018-02-22 17:47:06 -0500

    This is just a start, making a location class would be better

--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -3,10 +3,15 @@ package tests;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import code.Board;
+
 public class BoardTest {
 
 	@Test
-	public void test() {
+	public void testLocationIsValid() {
+		Board b = new Board();
+		assertEquals(9,b.getRedCount());
+		assertEquals(8,b.getBlueCount());
 		fail("Not yet implemented");
 	}
 

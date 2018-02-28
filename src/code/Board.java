61152d5c170cfd71c8d21e823f6a5b08222ad99c
judymei <judymei@10.84.140.228>
2018-02-28 16:07:56 -0500

    More test Cases Added

--- src/code/Board.java
+++ src/code/Board.java
@@ -4,28 +4,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import code.ManageTurns;
 import code.Location;
 
 public class Board {
 
+	/** 
+	 * New ManageTurns instance for this class
+	 */
 	private ManageTurns m = new ManageTurns();
 	/**
 	 * ArrayList of 25 Location instances
 	 */
-	private ArrayList<Location> board = new ArrayList<>();
+	private ArrayList<Location> board;
 	/**
-	 * The amount of agents left on the board;
+	 * The amount of red agents left on the board;
 	 */
-	//Needs to be instantiated in the constructor to 9
+	
 	private int redCount;
 	/**
 	 *The amount of blue agents left on the board. 
 	 */
-	//needs to be instantiated in the constructor to 8
+
 	private int blueCount;
+	/**
+	 * Constructor that initializes redCount and blueCount. 
+	 * Instantiates the board ArrayList
+	 */
+	public Board() {
+		redCount = 9;
+		blueCount = 8;
+		board = new ArrayList<Location>();
+	}
 	
 	public ArrayList<String> readFile(String filename) {
 		ArrayList<String> allNames = new ArrayList<String> ();
@@ -77,11 +88,15 @@ public class Board {
 		
 		return correctTeam;
 	}
-	
+	/**
+	 * @return Returns the amount of red agents left on the board
+	 */
 	public int getRedCount() {
 		return redCount;
 	}
-	
+	/**
+	 * @return Returns the amount of blue agents left on the board
+	 */
 	public int getBlueCount() {
 		return blueCount;
 	}
--- src/tests/BoardTest.java
+++ src/tests/BoardTest.java
@@ -20,7 +20,6 @@ public class BoardTest {
 	public void first() {
 		b = new Board();
 		_location = new ArrayList<>();
-//		b.setBoard(_location);
 		m = new ManageTurns();
 		Location l1 = new Location("apple", 1, true);
 		Location l2 = new Location("cat", 0, false);
@@ -28,14 +27,32 @@ public class BoardTest {
 		_location.add(l1);
 		_location.add(l2);
 		_location.add(l3);
+		b.setBoard(_location);
 	}
 
 	@Test
 	public void testLocationIsValid() {
 		m.setPlayer(1);
-		b.setBoard(_location);
 //		assertFalse(b.locationIsValid(_location.get(0), _location.get(0).getCodename()));
 		assertTrue(b.locationIsValid(_location.get(1), _location.get(1).getCodename()));
+	}
+	
+	@Test
+	public void testDecreaseBlue() {
+		b.locationIsValid(_location.get(0), _location.get(0).getCodename());
+		int blueCount = b.getBlueCount();
+		int redCount = b.getRedCount();
+		assertEquals("Method should decrease count if location is blue",7, blueCount);
+		assertEquals("Method should only decrease the count of the correct team",9, redCount);
+	}
+	
+	@Test
+	public void testDecreaseRed() {
+		b.locationIsValid(_location.get(1), _location.get(1).getCodename());
+		int blueCount = b.getBlueCount();
+		int redCount = b.getRedCount();
+		assertEquals("Method should decrease count if location is Red",8, blueCount);
+		assertEquals("Method should decrease count only if location is Red",8, redCount);
 	}
 	
 }

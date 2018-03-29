38be8563d7744fc25e85f4cf0562b6b3969f0bb8
Mekjm <Mekjm@10.84.117.131>
2018-03-29 17:34:49 -0400

    PlayerGUI

--- src/gui/PlayerGui.java
+++ src/gui/PlayerGui.java
@@ -1,5 +1,38 @@
 package gui;
 
-public class PlayerGui {
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
+import code.Board;
+
+public class PlayerGui {
+	private Board _board;
+	private main.Driver _driver;
+	private JPanel _mainPanel;
+	private JPanel _messagePanel;
+	private JTextArea msg;
+	
+	public PlayerGui(Board b, JPanel mp, main.Driver d) {
+		_driver = d;
+		_board = b;
+		
+		_mainPanel = mp;
+		_mainPanel.setLayout(new BoxLayout(_mainPanel, BoxLayout.Y_AXIS));
+		
+		_messagePanel = new JPanel();
+		_mainPanel.add(_messagePanel);
+		msg = new JTextArea();
+		_messagePanel.add(msg);
+		msg.setEditable(false);
+		msg.setText("Welcome");
+		//FIX LATER
+		if(_board.goodClue("") == false) {
+			msg.setText("Clue is illegal, Try Again!");
+		}
+		else {
+			msg.setText("Good Clue!");
+		}
+
+	}
 }

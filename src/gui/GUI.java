62772fc684dbc9d4265e8da3321706ecbb9cd070
Mekjm <Mekjm@10.84.119.20>
2018-04-01 13:44:03 -0400

    Peer Programming Josh and Justin
    Added a File Menu

--- src/gui/GUI.java
+++ src/gui/GUI.java
@@ -1,20 +1,31 @@
 package gui;
 
+import java.awt.Component;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JTextArea;
 
 import GUIEventHandlers.SwitchTurn;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import code.Board;
 import code.ManageTurns;
 
-public class GUI {
+public class GUI extends JFrame implements ActionListener {
 	private Board _board;
 	private main.Driver _driver;
 	private JPanel _mainPanel;
@@ -24,13 +35,53 @@ public class GUI {
 	private ManageTurns _m = new ManageTurns();
 	private JPanel _cardPanel;
 	private JPanel _switchPanel;
+	private JPanel _filePanel;
+	private GetUserInput _getUserInput;
+	
+	// JMenu stuff
+	JMenuBar menuBar;
+	JMenu menu, submenu;
+	JMenuItem menuItem;
+	JRadioButtonMenuItem rbMenuItem;
+	JCheckBoxMenuItem cbMenuItem;
+	
+
+	   public void actionPerformed(ActionEvent ae) { 
+	      String choice = ae.getActionCommand(); 
+	      if (choice.equals("Start")) {
+	    	  	_board.startGame();
+	      }
+	      else if (choice.equals("Quit")) { 
+	         System.exit(0); 
+	      } 
+	   } 
 	
 	public GUI(Board b, JPanel mp, main.Driver d) {
+		super(""); 
+
 		_driver = d;
 		_board = b;
 		
 		_mainPanel = mp;
 		_mainPanel.setLayout(new BoxLayout(_mainPanel, BoxLayout.Y_AXIS));
+		
+		// File 
+//		 setBounds(40,40,600,480); 
+//	      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	      JMenuBar menuBar = new JMenuBar(); 
+	      menuBar.setLayout(new BoxLayout(menuBar, BoxLayout.X_AXIS));
+	      menuBar.setAlignmentX(Component.RIGHT_ALIGNMENT);
+	      setJMenuBar(menuBar); 
+	      JMenu file = new JMenu("File"); 
+	      JMenuItem start = new JMenuItem("Start"); 
+	      start.addActionListener(this); 
+	      file.add(start); 
+	      menuBar.add(file); 
+	      JMenuItem quit = new JMenuItem("Quit"); 
+	      quit.addActionListener(this); 
+	      file.add(quit); 
+	      _mainPanel.add(menuBar);
+		
 		
 		_messagePanel = new JPanel();
 		_mainPanel.add(_messagePanel);
@@ -80,6 +131,9 @@ public class GUI {
 		_switchPanel.add(spymasterButton);
 		_switchPanel.add(playerButton);
 		_mainPanel.add(_switchPanel);
+		
+		// IF BELOW IS UNCOMMENTED, IT BREAKS EVERYTHING!!!
+//		_getUserInput.input();
 
 		
 

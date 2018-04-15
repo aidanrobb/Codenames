936c30fc777371db78c58c5a2fa4dcdb2a94c63c
joycesom <33379415+joycesom@users.noreply.github.com>
2018-04-15 13:31:12 -0400

    added javadoc to driver class

--- src/main/Driver.java
+++ src/main/Driver.java
@@ -11,22 +11,48 @@ import gui.GUI;
 
 public class Driver implements Runnable{
 
+	/* 
+	 * Board for this class to access its methods.
+	 */
 	private Board _board;
+	
 //	private Location _location;
 //	private ManageTurns _manageTurns;
+	
+	/*
+	 * JFrame to run game in.
+	 */
 	private JFrame _window;
+	
+	/*
+	 * JPanel to store functionality of the game in.
+	 */
 	private JPanel _mainPanel;
 	
+	/*
+	 * Drive to instantiate the board class in main method.
+	 */
 	public Driver(Board b) {
 		_board = b; 
 //		_location = l;
 //		_manageTurns = m;
 	}
 	
+	/*
+	 * Main method to run program.
+	 * 
+	 * @param board object to use
+	 */
 	public static void main(String[] args) {
 		Board b = new Board();
 		SwingUtilities.invokeLater(new Driver(b));
 	}
+	
+	/*
+	 * Instantiates the GUI methods and makes it so that the windows and functionality is visible.
+	 * 
+	 * @param standard String[] args array
+	 */
 	@Override
 	public void run() {
 		_window = new JFrame("Code Names");
@@ -41,6 +67,9 @@ public class Driver implements Runnable{
 		
 	}
 
+	/*
+	 * updates the JFrame, _window, to the latest version.
+	 */
 	public void updateJFrame() {
 		_window.pack();
 		_window.repaint();
